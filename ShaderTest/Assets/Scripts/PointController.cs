48067cd7c75c6c4ebf6ef107d50ead42ad6080f2
gowest2323 <35441974+gowest2323@users.noreply.github.com>
2018-04-25 03:08:38 +0900

    points

---
+++ ShaderTest/Assets/Scripts/PointController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+    //private Vector3[] vectorList;
+
+    //private Vector3 pos;
+
+    [SerializeField]
+    Material targetMat;
+
+    //public GameObject obj;
+    bool isClick = false;
+    bool isPoint = false;
+    private float range = 0;
+
+
+
+    void Start()
+    {
+        //MeshFilter meshFilter = GetComponent<MeshFilter>();
+        //meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Points, 0);
+
+
+        TurnMeshToTriangle();
+        //TurnMeshToPoint();
+
+    }
+
+    public void TurnMeshToPoint()
+    {
+
+        if (GetComponent<SkinnedMeshRenderer>())
+        {
+            SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+            smr.sharedMesh.SetIndices(smr.sharedMesh.GetIndices(0), MeshTopology.Points, 0);
+        }
+        else if(GetComponent<MeshFilter>())
+        {
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Points, 0);
+        }
+    }
+
+    public void TurnMeshToTriangle()
+    {
+        if(GetComponent<SkinnedMeshRenderer>())
+        {
+            SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+            smr.sharedMesh.SetIndices(smr.sharedMesh.GetIndices(0), MeshTopology.Triangles, 0);
+        }
+        else if (GetComponent<MeshFilter>())
+        {
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Triangles, 0);
+        }
+    }
+
+
+    public void LetsBlow()
+    {
+        TurnMeshToPoint();
+        isClick = true;
+        isPoint = true;
+
+    }
+
+    private void Update()
+    {
+
+
+        if (isClick)
+        {
+            range += 0.05f;
+        }
+
+        targetMat.SetFloat("_Displacement", range);
+    }
+
+}
--- ShaderTest/Assets/Shaders/Gradation.shader
+++ ShaderTest/Assets/Shaders/Gradation.shader
@@ -2,7 +2,9 @@
 {
 	Properties
 	{
+        _MainTex("Texture", 2D) = "white" {}
 		_Color("Color", Color) = (1, 1, 1, 1)
+        _Displacement("Displacement", Range(0,10)) = 0
 	}
 	SubShader
 	{
@@ -14,45 +16,40 @@
 		Pass
 		{
 			CGPROGRAM
+
 			#pragma vertex vert
 			#pragma fragment frag
 			
 			#include "UnityCG.cginc"
 
-			struct appdata
-			{
-				float4 vertex : POSITION;
-				float2 uv : TEXCOORD0;
-			};
-
 			struct v2f
 			{
-				float2 uv : TEXCOORD0;
-				UNITY_FOG_COORDS(1)
-				float4 vertex : SV_POSITION;
+				float4 pos : SV_POSITION;
+                float3 uv : TEXCOORD0;
+                //float4 col : COLOR;
 			};
 
-			sampler2D _MainTex;
-			float4 _MainTex_ST;
+            sampler2D _MainTex;
+            float4 _Color;
+            float _Displacement;
 			
-			v2f vert (appdata v)
+			v2f vert (appdata_base v)
 			{
-				v2f o;
-				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-				UNITY_TRANSFER_FOG(o,o.vertex);
+                v2f o;
+                float3 n = UnityObjectToWorldNormal(v.normal);
+                o.pos = UnityObjectToClipPos(v.vertex) + float4(n * _Displacement, 0);
+                o.uv = mul(unity_ObjectToWorld, v.vertex).xyz;
 				return o;
 			}
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
-				// sample the texture
-				fixed4 col = tex2D(_MainTex, i.uv);
-				// apply fog
-				UNITY_APPLY_FOG(i.fogCoord, col);
-				return col;
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                return col * _Color;
 			}
 			ENDCG
 		}
 	}
+    FallBack Off
 }
