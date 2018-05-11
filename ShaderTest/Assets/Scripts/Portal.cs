83c761f2f75db8a3ef70108b6ee0ea1e18afebec
gowest2323 <35441974+gowest2323@users.noreply.github.com>
2018-05-12 00:07:48 +0900

    Portal

---
+++ ShaderTest/Assets/Scripts/Portal.cs
@@ -0,0 +1,67 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    Material material;
+    [SerializeField]
+    Texture texture;
+    [SerializeField]
+    float radius = 0.15f;
+
+    readonly int subTexPropertyId = Shader.PropertyToID("_SubTex");
+
+    void Start()
+    {
+        material.SetTexture(subTexPropertyId, texture);
+    }
+
+    void Update()
+    {
+        var mousePosition = Input.mousePosition;
+
+        var uv = new Vector3(
+            mousePosition.x / Screen.width,
+            mousePosition.y / Screen.height, 0);
+
+        material.SetVector("_Position", uv);
+        material.SetFloat("_Aspect", Screen.height / (float)Screen.width);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OpenPortal();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ClosePortal();
+        }
+    }
+
+    float currentPortalRadius = 0;
+    void OpenPortal()
+    {
+        //SetPortalRadius(radius);
+        DOTween.KillAll();
+        DOTween.To(() => currentPortalRadius, SetPortalRadius, radius, 2f).SetEase(Ease.OutBack);
+    }
+
+    void ClosePortal()
+    {
+        //SetPortalRadius(0);
+        DOTween.KillAll();
+        DOTween.To(() => currentPortalRadius, SetPortalRadius, 0f, 0.6f).SetEase(Ease.InBack);
+    }
+
+    readonly int radiusPropertyId = Shader.PropertyToID("_Radius");
+    void SetPortalRadius(float radius)
+    {
+        currentPortalRadius = radius;
+        material.SetFloat(radiusPropertyId, radius);
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+}
---
+++ ShaderTest/Assets/Shaders/Protal.shader
@@ -0,0 +1,59 @@
+﻿Shader "Custom/Portal"
+{
+    Properties
+    {
+        _MainTex("MainTex", 2D) = "black"{}
+    }
+
+    CGINCLUDE
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    sampler2D _SubTex;
+    float _Aspect;
+    float _Radius;
+    float2 _Position;
+
+    // Transform UV for Portal Effect
+    // .xy = uv for portal effect
+    // .z  = 1: inside, 0: outside
+    float3 transform_uv(float2 uv)
+    {
+        float width = 0.07;
+
+        // 自身のピクセルからポータル中心までの距離
+        float distance = length((_Position - uv) * float2(1, _Aspect));
+
+        // 自身のピクセル位置での歪み具合
+        float distortion = 1 - smoothstep(_Radius - width, _Radius, distance);
+
+        // 自身のピクセル位置での歪み具合分だけ
+        // ポータル中心の方へずらした uv を計算します
+        uv += (_Position - uv) * distortion;
+
+        return float3(uv, step(1, distortion));
+    }
+
+    float4 frag(v2f_img i) : SV_Target
+    {
+        float3 portal = transform_uv(i.uv);
+        // 計算した uv で _MainTex のカラーを出力します
+        // ポータル内に違う絵を出すために、
+        // lerp + step で出力テクスチャを切り替えています
+        return lerp(tex2D(_MainTex, portal.xy),
+            tex2D(_MainTex, i.uv),
+            portal.z);
+    }
+    ENDCG
+
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+}
